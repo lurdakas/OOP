029bcd209aee644b46edb3a6107b025b0f2ee2dc
lurdakas <lkasileviciute@protonmail.com>
2024-05-24 14:33:15 +0300

    Task with Books finished.

---
+++ src/Book.java
@@ -0,0 +1,46 @@
+public class Book {
+
+
+    private String title;
+    private int pages;
+    private int releaseYear;
+
+    // empty constructor
+    public Book() {
+
+    }
+    // full constructor
+
+    public Book(String title, int pages, int releaseYear) {
+        this.title = title;
+        this.pages = pages;
+        this.releaseYear = releaseYear;
+    }
+
+    // getter
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+        public int getPages() {
+        return pages;
+    }
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+    public int getReleaseYear() {
+        return this.releaseYear;
+    }
+    public void fullname() {
+        System.out.println("Book Ttile: " + this.title + " Pages: " + this.pages + " Realed in: " + this.releaseYear);
+    }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,47 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Book book1 = new Book();
+        book1.setTitle("The Fellowship of the Ring");
+        book1.setPages(423);
+        book1.setReleaseYear(1954);
+        System.out.println(book1);
+
+
+        Book book2 = new Book();
+        book2.setTitle("The Two Towers");
+        book2.setPages(352);
+        book2.setReleaseYear(1955);
+        System.out.println(book2);
+
+
+        Book book3 = new Book();
+        book3.setTitle("The Return of the King");
+        book3.setPages(464);
+        book3.setReleaseYear(1955);
+        System.out.println(book3);
+
+
+
+        Book book4 = new Book("Hobbit", 320, 1937);
+        Book book5 = new Book("Murder On The Orient Express", 222, 1934);
+        Book book6 = new Book("Dead Poets Society", 176,1988);
+
+
+        book1.fullname();
+        book2.fullname();
+        book3.fullname();
+
+        book4.fullname();
+        book5.fullname();
+        book6.fullname();
+
+
+
+
 
     }
 }
